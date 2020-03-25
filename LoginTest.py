45d23ce07abf4fddddd9d4b2051a18b284a45d2b
tinpham943 <40230854+tinpham943@users.noreply.github.com>
2020-03-25 22:21:36 +0700

    Update LoginTest.py

--- LoginTest.py
+++ LoginTest.py
@@ -3,16 +3,12 @@ from selenium import webdriver
 
 def main():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_experimental_option("detach", True)
     chrome_options.add_argument("--disable-infobars")
-    chrome_options.add_experimental_option("prefs", {
-        "profile.default_content_setting_values.notifications": 2  # 1:allow, 2:block
-    })
+    chrome_options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(30)  # seconds
 
-    # Go to facebook.com
     driver.get("http://newtours.demoaut.com/")
     userName = driver.find_element_by_name('userName')
     passWord = driver.find_element_by_name('password')
@@ -24,6 +20,7 @@ def main():
     assert (driver.find_element_by_xpath(
         '/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td/img').is_displayed() == True)
     print('Passed')
+    driver.close()
 
 
 if __name__ == '__main__':
